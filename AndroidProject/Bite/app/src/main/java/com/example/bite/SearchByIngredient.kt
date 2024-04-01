2f27eb888ededf527a9656dd9bc7b191d983190c
akifbayram <56325239+akifbayram@users.noreply.github.com>
2024-04-01 18:48:31 -0400

    Update Search by Ingredient UI, Add Search FAB Replace EditText with SearchView Customize SearchView with colored background and rounded corners Make the entire SearchView clickable Change the search button to a floating action button

--- AndroidProject/Bite/app/src/main/java/com/example/bite/SearchByIngredient.kt
+++ AndroidProject/Bite/app/src/main/java/com/example/bite/SearchByIngredient.kt
@@ -2,68 +2,70 @@ package com.example.bite
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bite.models.IngredientResponse
 import com.example.bite.network.SpoonacularRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-
-class SearchByIngredient: AppCompatActivity() {
+class SearchByIngredient : AppCompatActivity() {
     private lateinit var spoonacularRepository: SpoonacularRepository
     private lateinit var recyclerView: RecyclerView
     private lateinit var selectedRecyclerView: RecyclerView
     private lateinit var ingredientAdapter: IngredientAdapter
     private lateinit var selectedAdapter: IngredientAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ingredient_search)
 
-
-
         // TODO: Get IDs
-        val searchButton = findViewById<Button>(R.id.SubmitSearchButton)
-        val exitButton = findViewById<Button>(R.id.exit)
+        val searchView = findViewById<SearchView>(R.id.SearchInput)
+        val exitButton = findViewById<ImageButton>(R.id.exit)
         recyclerView = findViewById(R.id.recyclerViewCommonIngredients)
         selectedRecyclerView = findViewById(R.id.recyclerViewSelectedIngredients)
+
         ingredientAdapter = IngredientAdapter(emptyList())
         selectedAdapter = IngredientAdapter(emptyList())
 
-        recyclerView.layoutManager = GridLayoutManager(this,4)
+        recyclerView.layoutManager = GridLayoutManager(this, 4)
         recyclerView.adapter = ingredientAdapter
 
         spoonacularRepository = SpoonacularRepository()
-        // TODO: Use Button To Search Ingredient List
-        searchButton.setOnClickListener{
-            val IngredientInput = findViewById<EditText>(R.id.SearchInput)
-            val query = IngredientInput.text.toString()
-            searchIngredientByName(query)
 
-        }
+        // TODO: Use SearchView to search ingredient list
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                searchIngredientByName(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                // search suggestions as the user types
+                return true
+            }
+        })
+
         var selected: List<IngredientResponse> = emptyList()
-        selectedRecyclerView.layoutManager = GridLayoutManager(this,4)
+        selectedRecyclerView.layoutManager = GridLayoutManager(this, 4)
         selectedRecyclerView.adapter = selectedAdapter
-        ingredientAdapter.setOnClickListener (object :
-            IngredientAdapter.OnClickListener {
-                override fun onClick(position: Int, ingredient: IngredientResponse) {
-                    selected = selected + ingredient
-                    selectedAdapter.updateIngredients(selected)
-                }
-            })
 
+        ingredientAdapter.setOnClickListener(object : IngredientAdapter.OnClickListener {
+            override fun onClick(position: Int, ingredient: IngredientResponse) {
+                selected = selected + ingredient
+                selectedAdapter.updateIngredients(selected)
+            }
+        })
 
         // TODO: Use Button To Exit Detail
-        exitButton.setOnClickListener{
+        exitButton.setOnClickListener {
             finish()
         }
     }
-
     private fun searchIngredientByName(query: String) {
         lifecycleScope.launch {
             try {
