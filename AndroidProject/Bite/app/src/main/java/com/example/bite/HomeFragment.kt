5b906b89923978886dae87268aa8216b2c3bf665
akifbayram <56325239+akifbayram@users.noreply.github.com>
2024-04-05 00:59:31 -0400

    Implement snapping to fragment_home discover

--- AndroidProject/Bite/app/src/main/java/com/example/bite/HomeFragment.kt
+++ AndroidProject/Bite/app/src/main/java/com/example/bite/HomeFragment.kt
@@ -13,6 +13,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.bite.adapters.HomeRecipeAdapter
@@ -45,6 +46,9 @@ class HomeFragment : Fragment() {
             startActivity(intent)
         }
         recipesRv.adapter = homeRecipeAdapter
+
+        val snapHelper = PagerSnapHelper() // or LinearSnapHelper()
+        snapHelper.attachToRecyclerView(recipesRv)
 
         rotdImageView = view.findViewById(R.id.rotdImageView)
         rotdTitleTextView = view.findViewById(R.id.rotdTitleTextView)
