bd1e427ddf163d62422e4039f1aae24469404d5e
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-07 12:33:41 -0400

    added back button functionality

--- AndroidProject/Bite/app/src/main/java/com/example/bite/DiscoverFragment.kt
+++ AndroidProject/Bite/app/src/main/java/com/example/bite/DiscoverFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -31,8 +32,15 @@ class DiscoverFragment : Fragment() {
         setupRecyclerView(view)
         setupScrollListener()
         fetchRecipesPaginated()
+
+        val backButton = view.findViewById<ImageButton>(R.id.exit)
+        backButton.setOnClickListener {
+            requireActivity().onBackPressed()
+        }
+
         return view
     }
+
 
     private fun setupRecyclerView(view: View) {
         recyclerView = view.findViewById(R.id.discoverCardRecycler)
