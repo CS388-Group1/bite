de05c0c7da0ce9bd474aaaa4dca38d551acdd047
akifbayram <56325239+akifbayram@users.noreply.github.com>
2024-04-15 15:24:48 -0400

    Update search results limit and pantry option in SpoonacularApi

--- AndroidProject/Bite/app/src/main/java/com/example/bite/network/SpoonacularApi.kt
+++ AndroidProject/Bite/app/src/main/java/com/example/bite/network/SpoonacularApi.kt
@@ -35,10 +35,10 @@ interface SpoonacularApi {
     @GET("recipes/findByIngredients")
     suspend fun searchRecipesByIngredients(
         @Query("ingredients") ingredients: String,
-        @Query("number") number: Int = 10,
+        @Query("number") number: Int = 50,
         @Query("limitLicense") limitLicense: Boolean = true,
         @Query("ranking") ranking: Int = 1,
-        @Query("ignorePantry") ignorePantry: Boolean = true,
+        @Query("ignorePantry") ignorePantry: Boolean = false,
         @Query("offset") offset: Int = 0,
         @Query("apiKey") apiKey: String = BuildConfig.SPOONACULAR_API_KEY
     ): List<RecipeResponse>
@@ -47,13 +47,13 @@ interface SpoonacularApi {
     @GET("food/ingredients/search")
     suspend fun searchIngredientByName(
         @Query("query") query: String,
-//        @Query("number") number: Int = 10,
+        @Query("number") number: Int = 50,
         @Query("apiKey") apiKey: String = BuildConfig.SPOONACULAR_API_KEY
     ): IngredientListResponse
 
     @GET("recipes/random")
     suspend fun getTrendingRecipes(
-//        @Query("number") number: Int = 10,
+        @Query("number") number: Int = 50,
         @Query("tags") tags: String = "vegetarian",
         @Query("apiKey") apiKey: String = BuildConfig.SPOONACULAR_API_KEY
     ): RecipeListResponse
@@ -68,7 +68,7 @@ interface SpoonacularApi {
     @GET("recipes/complexSearch")
     suspend fun searchRecipeByName(
         @Query("query") query: String,
-//        @Query("number") number: Int = 10,
+        @Query("number") number: Int = 50,
         @Query("apiKey") apiKey: String = BuildConfig.SPOONACULAR_API_KEY
     ): RecipeListResponse
 
