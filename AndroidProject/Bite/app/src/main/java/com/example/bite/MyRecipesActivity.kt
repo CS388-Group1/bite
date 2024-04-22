c7c77c751f4df05f6730f8b821d0ad229841187e
palakagr34 <palakagr34@gmail.com>
2024-04-22 16:50:29 -0400

    loading my recipes from firestore + fetching full correct recipe detail from firestore

--- AndroidProject/Bite/app/src/main/java/com/example/bite/MyRecipesActivity.kt
+++ AndroidProject/Bite/app/src/main/java/com/example/bite/MyRecipesActivity.kt
@@ -1,6 +1,9 @@
 package com.example.bite
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -10,13 +13,14 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.bite.daos.CustomRecipeDao
 import com.example.bite.models.CustomRecipeViewModel
 import com.example.bite.models.Recipe
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 class MyRecipesActivity : AppCompatActivity() {
@@ -64,27 +68,43 @@ class MyRecipesActivity : AppCompatActivity() {
             CoroutineScope(Dispatchers.IO).launch {
                 try {
                     val customRecipeDao = AppDatabase.getInstance(this@MyRecipesActivity).customRecipeDao()
-                    val customRecipes = customRecipeDao.getCustomRecipesWithIngredientsByUserId(userId)
-                    val recipeList = customRecipes.map { customRecipeWithIngredients ->
-                        Recipe(
-                            id = customRecipeWithIngredients.recipe.recipeId.toString(), // Set the ID as a string
-                            userId = userId,
-                            title = customRecipeWithIngredients.recipe.name,
-                            image = customRecipeWithIngredients.recipe.image,
-                            cookingTime = customRecipeWithIngredients.recipe.readyInMinutes,
-                            summary = "",
-                            sourceName = "",
-                            isFavorite = false
-                        )
-                    }
-                    withContext(Dispatchers.Main) {
-                        if (recipeList.isEmpty()) {
-                            noRecipesTextView.visibility = View.VISIBLE
-                            recyclerView.visibility = View.GONE
-                        } else {
-                            noRecipesTextView.visibility = View.GONE
-                            recyclerView.visibility = View.VISIBLE
-                            adapter.updateRecipes(recipeList)
+                    if (isConnectedToInternet(this@MyRecipesActivity)) {
+                        // Fetch recipes from Firestore
+                        val firestoreRecipes = fetchRecipesFromFirestore(userId)
+                        withContext(Dispatchers.Main) {
+                            updateUIWithRecipes(firestoreRecipes)
+                        }
+                    }else {
+
+                        val customRecipes =
+                            customRecipeDao.getCustomRecipesWithIngredientsByUserId(userId)
+                        val recipeList = customRecipes.map { customRecipeWithIngredients ->
+                            val imagePath =
+                                if (customRecipeWithIngredients.recipe.image.isNullOrEmpty()) {
+                                    "android.resource://com.example.bite/drawable/cookie_transparent_wide"
+                                } else {
+                                    customRecipeWithIngredients.recipe.image
+                                }
+                            Recipe(
+                                id = customRecipeWithIngredients.recipe.recipeId.toString(), // Set the ID as a string
+                                userId = userId,
+                                title = customRecipeWithIngredients.recipe.name,
+                                image = imagePath,
+                                cookingTime = customRecipeWithIngredients.recipe.readyInMinutes,
+                                summary = "",
+                                sourceName = "",
+                                isFavorite = false
+                            )
+                        }
+                        withContext(Dispatchers.Main) {
+                            if (recipeList.isEmpty()) {
+                                noRecipesTextView.visibility = View.VISIBLE
+                                recyclerView.visibility = View.GONE
+                            } else {
+                                noRecipesTextView.visibility = View.GONE
+                                recyclerView.visibility = View.VISIBLE
+                                adapter.updateRecipes(recipeList)
+                            }
                         }
                     }
                 } catch (e: Exception) {
@@ -96,8 +116,63 @@ class MyRecipesActivity : AppCompatActivity() {
         }
     }
 
+    private fun isConnectedToInternet(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager?
+        return connectivityManager?.activeNetworkInfo?.isConnected ?: false
+    }
 
+    private suspend fun fetchRecipesFromFirestore(userId: String): List<Recipe> {
+        val firestoreRecipes = mutableListOf<Recipe>()
 
+        try {
+            val firestore = FirebaseFirestore.getInstance()
+            val querySnapshot = firestore
+                .collection("users")
+                .document(userId)
+                .collection("createdRecipes")
+                .get()
+                .await()
+
+            for (document in querySnapshot.documents) {
+                val recipeId = document.id
+                val name = document.getString("name") ?: ""
+                val image = document.getString("image") ?: ""
+                val servings = document.getLong("servings") ?: 0
+                val readyInMinutes = document.getLong("readyInMinutes") ?: 0
+                val instructions = document.getString("instructions") ?: ""
+
+                // Create a Recipe object using the retrieved data
+                val recipe = Recipe(
+                    id = recipeId,
+                    userId = userId,
+                    title = name,
+                    image = image,
+                    cookingTime = readyInMinutes.toInt(),
+                    instructions = instructions,
+                    summary = "",
+                    sourceName = "",
+                    isFavorite = false
+                )
+
+                firestoreRecipes.add(recipe)
+            }
+        } catch (e: Exception) {
+            // Handle any exceptions, such as Firestore exceptions
+            // Log the error or handle it appropriately
+        }
+
+        return firestoreRecipes
+    }
+    private fun updateUIWithRecipes(recipeList: List<Recipe>) {
+        if (recipeList.isEmpty()) {
+            noRecipesTextView.visibility = View.VISIBLE
+            recyclerView.visibility = View.GONE
+        } else {
+            noRecipesTextView.visibility = View.GONE
+            recyclerView.visibility = View.VISIBLE
+            adapter.updateRecipes(recipeList)
+        }
+    }
     override fun onBackPressed() {
         super.onBackPressed()
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
